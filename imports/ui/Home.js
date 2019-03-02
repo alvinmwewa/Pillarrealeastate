b4f269c1555573d11d894ffeb24cd2b8817e107c
Alvin <mwewamwewa1699@gmail.com>
2019-03-02 23:03:09 +0200

    first commit

---
+++ imports/ui/Home.js
@@ -0,0 +1,21 @@
+import React, {Component } from 'react';
+
+class Home extends Component {
+
+	render(){
+	return (
+		<div>
+		<div class="jumbotron text-center">
+		<h1>Company</h1> 
+		<p>We specialize in blablabla</p> 
+		
+	  </div>
+	  </div>
+
+	)
+	}
+}
+
+export default Home
+
+
