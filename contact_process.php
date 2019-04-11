a3e1cfd0edb717cfa5a63ce702abe695fd55a2f5
jimmy kamanga <jimmykamanga@gmail.com>
2019-04-11 13:38:02 +0200

    Contact backend for the site

---
+++ contact_process.php
@@ -0,0 +1,39 @@
+
+<?php
+
+    $to = "pillarrealestate@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $cmessage = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "You have a review messege form Pillarrealestate.com";
+
+    $logo = 'asserts/images/aw.png';
+    $link = 'www.jansivisions.com';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td style='border: none'><strong>Message:</strong></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
+<?php
+echo "<script language='JavaScript'>alert(`$name your message has been sent successfully. \n \n Thank you for taking the time to write to us, we will get back to you as soon as possible. \n Feel free to sign-up for our news letters and follow our facebook page for faster updates on offers and promotions`); window.location = `/index.php`</script>"
+?>
